30643d0a7c49506faa233ecba493ad9ac45a073b
aav0038 <45108471+aav0038@users.noreply.github.com>
2019-03-07 21:54:49 +0100

    Update with number of group label
    Grupo 01 + 3

--- O01/Billiards.java
+++ O01/Billiards.java
@@ -16,7 +16,7 @@ public class Billiards extends JFrame {
 	private Board board;
 
 	// TODO update with number of group label. See practice statement.
-	private final int N_BALL = 2;
+	private final int N_BALL = 4;
 	private Ball[] balls;
 
 	public Billiards() {
